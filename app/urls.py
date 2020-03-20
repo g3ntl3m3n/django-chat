061235f66a05d4a9f855a32135cfcc641449bde8
g3ntl3m3n <somethingsmeels@gmail.com>
2020-03-20 21:42:21 +0300

    aa

---
+++ app/urls.py
@@ -0,0 +1,12 @@
+
+from django.contrib import admin
+from django.urls import path, include
+
+from .views import index, room
+
+app_name = "app"
+urlpatterns = [
+    path('', index, name="index"),
+    path('<str:room_name>/', room, name='room'),
+
+]
