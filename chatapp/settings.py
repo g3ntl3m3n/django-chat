f389cc6489a9b81782a0a4f372ba89a47603cdd8
g3ntl3m3n <somethingsmeels@gmail.com>
2020-03-20 22:01:48 +0300

    consumers, test, selenium, dockerized, redis

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -62,6 +62,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'chatapp.wsgi.application'
 ASGI_APPLICATION = "chatapp.routing.application"
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
 
 
 # Database
@@ -71,6 +79,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'TEST': {
+            'NAME': os.path.join(BASE_DIR, 'db_test.sqlite3')
+        }
     }
 }
 
